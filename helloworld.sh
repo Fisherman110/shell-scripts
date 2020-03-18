7bdb1b89369d215dff60fc17dd8fbfb858444f55
Fisherman110 <2652134385@qq.com>
2020-03-18 21:15:00 +0800

    first scripts

---
+++ helloworld.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#program:to show helloworld
+#2020.3.18  by Fisherman110
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+echo -e "Helloworld ! \a \n"
+exit 0
