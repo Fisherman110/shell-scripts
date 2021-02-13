e807901709f49a41257f1f3732dcce845cf10e47
Fisherman110 <2652134385@qq.com>
2021-02-13 22:13:31 +0800

    测试可不可以直接在脚本输入命令

---
+++ commandtest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#program:to test weather you can just write commands in script to excute them
+#2021.2.13  by Fisherman110
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+ps aux >> temp.txt
+exit 0
