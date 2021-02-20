06a9073645495ca20b48568cd1346c4b146f05dc
Fisherman110 <2652134385@qq.com>
2021-02-20 17:34:24 +0800

    实际把桌面的文件备份到服务器

--- backup1.0.sh
+++ backup1.0.sh
@@ -5,15 +5,22 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 #echo -e "Helloworld ! \a \n"
 #DATE='date -d '1 days ago' +%Y%m%d'
-GAMEDIR='/home/littlebear/Pictures/Wallpapers/'
+GAMEDIR='/home/littlebear/Desktop'
 /usr/bin/ftp -n  <<!
 open 192.168.1.2
 user Fisherman110 1234567
 binary
 lcd $GAMEDIR
 prompt
-cd file/picture_back
-put abc-123.jpg
+cd file/txt_back
+put 养生.text
+put mysql命令.text
+put 运动.text
+put bash笔记.text
+put 命令行ftp.text
+put 常用日志.text
+put 目标.text
+put vi使用技巧.text
 exit
 bye
 !
