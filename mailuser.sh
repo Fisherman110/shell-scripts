08553fa00aa8a2daa791e368302ae4609328f27b
Fisherman110 <2652134385@qq.com>
2021-02-20 19:24:48 +0800

    linux给本地用户发邮件

--- mailuser.sh
+++ mailuser.sh
@@ -3,6 +3,6 @@
 #2021.2.200  by Fisherman110
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-mail littlebear> /home/littlebear/software/backup/log.txt
+mail littlebear< /home/littlebear/software/backup/log.txt
 
 exit 0
